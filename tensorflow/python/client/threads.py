bf2ceb2b1e8dfa048ce13e49231c80641ff607dd
Jongmin Baek <jongmin@dropbox.com>
2018-09-26 22:45:39 -0700

    Export pause_all_threads and resume_all_threads

--- tensorflow/python/client/threads.py
+++ tensorflow/python/client/threads.py
@@ -3,7 +3,9 @@ May be used to safely fork a process with live Session objects.
 """
 
 from tensorflow.python import pywrap_tensorflow
+from tensorflow.python.util.tf_export import tf_export
 
+@tf_export('pause_all_threads')
 def pause_all_threads():
     """Shut down all worker threads in TensorFlow thread pools, after waiting for work to complete.
     After this call returns, it is safe to fork the process. It is an error to schedule any work on
@@ -24,6 +26,7 @@ def pause_all_threads():
     """
     pywrap_tensorflow.TF_PauseAllThreads()
 
+@tf_export('resume_all_threads')
 def resume_all_threads():
     """Recreate all threads stopped by `tf.pause_all_threads`.
     Call this after forking and before running any session.
